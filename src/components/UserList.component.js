1abae8353807ce94e4d4819e78bc5f6520ddb9d6
Νικος Καραγιαννης <karamatros@yahoo.gr>
2021-12-15 14:27:42 +0200

    Added:Table and showed data

--- src/components/UserList.component.js
+++ src/components/UserList.component.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import UserListService from "../services/UsersService";
-
+import Table from "react-bootstrap/Table";
 export default function UsersList() {
   const [users, setUsers] = useState([]);
 
@@ -23,11 +23,41 @@ export default function UsersList() {
   return (
     <div>
       <h1>I Work</h1>
-      <ul>
-        {users.map((post) => (
-          <li key={post.id}>{post.title}</li>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Active</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              {/* <td>{user.active}</td> */}
+              <td>{user.type}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      {/* <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
+}
+
+{
+  /* <ul>
+  {users.map((user) => (
+    <li key={user.id}>{user.name}</li>
+  ))}
+</ul>; */
 }
--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -1,7 +1,7 @@
 import http from "../http-common";
 
 const getAll = () => {
-  return http.get("/posts");
+  return http.get("/users");
 };
 
 const update = (id, data) => {
